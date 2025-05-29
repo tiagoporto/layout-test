2036bf2e9fb49a4152e3f4852b9cccbfc616e665
Tiago Porto <tiago@tiagoporto.com>
2025-05-29 17:51:13 -0300

    Set Basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: process.env.CI ? "/layout-test" : "",
 };
 
 export default nextConfig;
